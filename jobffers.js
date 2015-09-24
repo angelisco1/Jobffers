6f155c51d2e4ea766925a0303705ec4347449edf
Angel Villalba <angelvil92@gmail.com>
2015-09-24 17:26:33 +0200

    Load offers from MongoDB

--- jobffers.js
+++ jobffers.js
@@ -1,107 +1,28 @@
-// Offers = new Mongo.Collection("offers");
+Offers = new Mongo.Collection("offers");
 
 if (Meteor.isClient) {
   Template.body.helpers({
-    offers: [
-      {
-        company: {
-          name: "BQ", 
-          url: "www.bq.com"
-        }, 
-        stack: "back", 
-        reasonDeclined: "", 
-        status: [
-            {
-              date: new Date(),
-              description: "Enviado curriculum",
-              personContacted: {
-                name: "alex",
-                email: "alex@bq.com",
-                rol: "PM"
-              },
-              answered: true
-            },
-            {
-              date: new Date(),
-              description: "Entrevista confirmada para el dia 22/04/15",
-              personContacted: {
-                name: "alex",
-                email: "alex@bq.com",
-                rol: "PM"
-              },
-              answered: false
-            }
-          ],
-        followup: {
-          done: false,
-          date: null
-        }
-      },
-      {
-        company: {
-          name: "Lextrend", 
-          url: "www.lextrend.com"
-        }, 
-        stack: "back", 
-        reasonDeclined: "", 
-        status: [
-            {
-              date: new Date(),
-              description: "Entrevista realizada, ya me llamaran",
-              personContacted: {
-                name: "carlos oca",
-                email: "carlos@lextrend.com",
-                rol: "CEO"
-              },
-              answered: false
-            }
-          ],
-        followup: {
-          done: false,
-          date: null
-        }
-      },
-      {
-        company: {
-          name: "Ideas4All", 
-          url: "www.ideas4all.com"
-        }, 
-        stack: "back", 
-        reasonDeclined: "", 
-        status: [
-            {
-              date: new Date(),
-              description: "Enviado curriculum",
-              personContacted: {
-                name: "alberto",
-                email: "alberto@ideas4all.com",
-                rol: "Recruiter"
-              },
-              answered: true
-            },
-            {
-              date: new Date(),
-              description: "Rechazado porque han cogido a alguien",
-              personContacted: {
-                name: "alberto",
-                email: "alberto@ideas4all.com",
-                rol: "Recruiter"
-              },
-              answered: true
-            }
-          ],
-        followup: {
-          done: true,
-          date: new Date()
-        }
-      }
-    ]
+    offers: function(){
+      return Offers.find({});
+    }
+  });
+  
+  Template.offer.events({
+    "click .delete": function(event){
+      Meteor.call("deleteOffer", this._id);
+    }
   });
 
-  
+  Meteor.methods({
+    deleteOffer: function(offerId){
+      Tasks.remove(offerId);
+    }
+  });
 
 }
 
 if (Meteor.isServer) {
-
+  Meteor.startup(function () {
+    
+  });
 }
