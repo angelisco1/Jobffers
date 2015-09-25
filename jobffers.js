a100d49de9c23153daa500cb5dfc0761a6aeb5af
Angel Villalba <angelvil92@gmail.com>
2015-09-25 17:53:01 +0200

    Add and delete offers

--- jobffers.js
+++ jobffers.js
@@ -1,25 +1,58 @@
 Offers = new Mongo.Collection("offers");
 
 if (Meteor.isClient) {
+
   Template.body.helpers({
     offers: function(){
       return Offers.find({});
     }
   });
-  
+
+  Template.body.events({
+    "submit form": function(event){
+      event.preventDefault();
+      var companyName = $(event.target).find('[name=companyName]').val();
+      var companyUrl = $(event.target).find('[name=companyUrl]').val();
+      var stack = $(event.target).find('[name=stack]').val();
+      Meteor.call("addOffer", companyName, companyUrl, stack);
+    },
+    "click .add-new-offer": function(event){
+      document.getElementById("form-new-offer").reset();
+    }
+  });
+
   Template.offer.events({
     "click .delete": function(event){
       Meteor.call("deleteOffer", this._id);
     }
   });
 
-  Meteor.methods({
-    deleteOffer: function(offerId){
-      Tasks.remove(offerId);
-    }
-  });
-
 }
+
+Meteor.methods({
+  addOffer: function(companyName, companyUrl, stack){
+    Offers.insert({
+      company: {
+        name: companyName,
+        url: companyUrl
+      },
+      stack: stack,
+      declined: {
+        isDeclined: false,
+        reason: ""
+      },
+      followup: {
+        done: false,
+        date: null
+      },
+      status: []
+    });
+  },
+  deleteOffer: function(offerId){
+    Offers.remove(offerId);
+  }
+});
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
